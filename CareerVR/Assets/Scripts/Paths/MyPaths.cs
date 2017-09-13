4b2a85c55c5e32de808541586310217f2589b9ed
sai25640 <sai25640@163.com>
2017-09-13 16:36:47 +0800

    master 2017/9/13 add fishPath

---
+++ CareerVR/Assets/Scripts/Paths/MyPaths.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class MyPaths : MonoBehaviour {
+
+    public Transform target;
+    public PathType pathType = PathType.CatmullRom;
+    public Transform[] waypoints = new Transform[] { };
+    public float duration = 5;
+ 
+    void Start ()
+    {
+        Vector3[] waypointsV3 = new Vector3[waypoints.Length];
+        for (int i = 0;i<waypoints.Length;i++)
+        {
+            waypointsV3[i] = waypoints[i].position;
+        }
+      
+        Tween t = target.DOPath(waypointsV3, duration, pathType)
+            .SetOptions(true)
+            .SetLookAt(0.001f);
+        t.SetEase(Ease.Linear).SetLoops(-1);
+    }
+	
+	
+}
