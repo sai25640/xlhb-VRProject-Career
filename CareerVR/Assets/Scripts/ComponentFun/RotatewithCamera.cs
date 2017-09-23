36d5bdc4d83e7c784affb474b017bcb558c4499a
sai25640 <sai25640@163.com>
2017-09-23 10:04:04 +0800

    master 2017/9/22 add xinjiaoshi

---
+++ CareerVR/Assets/Scripts/ComponentFun/RotatewithCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatewithCamera : MonoBehaviour {
+
+    public Camera camera;
+    //private float x,y,z;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        //x = camera.transform.localRotation.x;
+        //y = camera.transform.localRotation.y;
+        //z = camera.transform.localRotation.z;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+     
+        this.transform.forward = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+	}
+}
---
+++ CareerVR/Assets/Scripts/UI/TypewriterEffect.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+/// <summary>  
+/// 此脚本是能够将文本字符串随着时间打字或褪色显示。  
+/// </summary>  
+
+[RequireComponent(typeof(Text))]
+[AddComponentMenu("Typewriter Effect")]
+public class TypewriterEffect : MonoBehaviour
+{
+    public UnityEvent myEvent;
+    public int charsPerSecond = 0;
+    // public AudioClip mAudioClip;             // 打字的声音，不是没打一个字播放一下，开始的时候播放结束就停止播放  
+    private bool isActive = false;
+
+    private float timer;
+    private string words;
+    private Text mText;
+
+    void Start()
+    {
+        if (myEvent == null)
+            myEvent = new UnityEvent();
+
+        words = GetComponent<Text>().text;
+        GetComponent<Text>().text = string.Empty;
+        timer = 0;
+        isActive = true;
+        charsPerSecond = Mathf.Max(1, charsPerSecond);
+        mText = GetComponent<Text>();
+    }
+
+    void ReloadText()
+    {
+        words = GetComponent<Text>().text;
+        mText = GetComponent<Text>();
+    }
+
+    public void OnStart()
+    {
+        ReloadText();
+        isActive = true;
+    }
+
+    void OnStartWriter()
+    {
+        if (isActive)
+        {
+            try
+            {
+                mText.text = words.Substring(0, (int)(charsPerSecond * timer));
+                timer += Time.deltaTime;
+            }
+            catch (Exception)
+            {
+                OnFinish();
+            }
+        }
+    }
+
+    void OnFinish()
+    {
+        isActive = false;
+        timer = 0;
+        GetComponent<Text>().text = words;
+        try
+        {
+            myEvent.Invoke();
+        }
+        catch (Exception)
+        {
+            Debug.Log("问题");
+        }
+    }
+
+    void Update()
+    {
+        OnStartWriter();
+    }
+}
