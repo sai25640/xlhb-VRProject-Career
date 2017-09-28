3c3945b417310f83957d2706d9ac8fd4230c8a18
sai25640 <sai25640@163.com>
2017-09-28 17:27:40 +0800

    master 2017/9/28 add UIScene

--- CareerVR/Assets/Scripts/UI/UIManager.cs
+++ CareerVR/Assets/Scripts/UI/UIManager.cs
@@ -7,15 +7,26 @@ public class UIManager : GenericSingletonClass<UIManager> {
     public GameObject canvas;
     public Text text;
 
+    ////////////////////////////////////////////////////////深海场景//////////////////////////////////////////////////////////////
+    //显示物品信息
     public void Show(GameObject go)
     {
         text.text = go.GetComponent<ObjectInfo>().info;
         canvas.SetActive(true);
     }
 
+    //隐藏物品信息
     public void Hide()
     {
         text.text = "";
         canvas.SetActive(false);
+    }
+
+
+    ////////////////////////////////////////////////////////登陆场景//////////////////////////////////////////////
+
+    public void OnLoginButtonClick()
+    {
+        Debug.Log(" OnLoginButtonClick!");
     }
 }
---
+++ CareerVR/Assets/Scripts/UI/UIManagerForLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UIManagerForLogin: MonoBehaviour
+{
+
+    public GameObject loginPanel;
+    public GameObject selectScenePanel;
+
+    public void OnLoginButtonClick()
+    {
+        loginPanel.SetActive(false);
+        selectScenePanel.SetActive(true);
+    }
+
+    public void OnDeapSeaClick()
+    {
+        //Debug.Log(" OnDeapSeaClick!");
+        SceneManager.LoadScene("DeepSea");
+    }
+}
